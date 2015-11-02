64fb38a1fc50f101a12a7836053d11d53fafa34a
sydorenkovd <sydorenkovd@gmail.com>
2015-11-02 11:57:41 +0200

    include new css and js files

--- assets/MyAppAsset.php
+++ assets/MyAppAsset.php
@@ -13,21 +13,11 @@ use yii\web\AssetBundle;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class AppAsset extends AssetBundle
+class MyAppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/ie.css'
-    ];
-	
-	public $cssOptions = ['conditon' => 'lte IE8'];
-	
-    public $js = [
-		'js/functions.js',
-		'//vk.com/js/api/openapi.js?63'
-    ];
-    public $depends = [
-        'app\assets\MyAppAsset'
+        'css/main.css',
     ];
 }
